b0990d1aef4758286cf9d4b402a946aca62fd592
Michael Hurwicz <mhurwicz@gmail.com>
2017-01-06 09:55:05 -0800

    choose int type from multiple choice

--- app/src/main/jni/SuperpoweredExample.h
+++ app/src/main/jni/SuperpoweredExample.h
@@ -10,6 +10,7 @@
 #include <SuperpoweredRoll.h>
 #include <SuperpoweredFlanger.h>
 #include <AndroidIO/SuperpoweredAndroidAudioIO.h>
+#include <jni.h>
 
 #define HEADROOM_DECIBEL 3.0f
 static const float headroom = powf(10.0f, -HEADROOM_DECIBEL * 0.025f);
