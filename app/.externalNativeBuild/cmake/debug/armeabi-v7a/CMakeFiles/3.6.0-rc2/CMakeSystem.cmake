85fae4a24f54bc6276d8bf77065251cfaf53ca32
Michael Hurwicz <mhurwicz@gmail.com>
2017-01-06 05:59:42 -0800

    first commit in branch

---
+++ app/.externalNativeBuild/cmake/debug/armeabi-v7a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-15.6.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "15.6.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/Users/mh/phonegap/android-sdk/adt-bundle-mac-x86_64-20131030/sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-16")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "16")
+set(CMAKE_SYSTEM_PROCESSOR "armv7-a")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ app/.externalNativeBuild/cmake/release/x86_64/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-15.6.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "15.6.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/Users/mh/phonegap/android-sdk/adt-bundle-mac-x86_64-20131030/sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-21")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "21")
+set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
